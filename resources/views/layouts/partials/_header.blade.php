8d19b60cf9051a7f6f4aabda126b1ac34d78b7fa
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 17:14:25 +0100

    up

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -10,6 +10,10 @@
     <div class="container">
       <nav>
         <ul>
-          <li><a hfref=""></a></li>
+          <li><a hfref="">Home</a></li>
+          <li><a hfref="">Corso</a></li>
+          <li><a hfref="">Dopo Corso</a></li>
+          <li><a hfref="">Lezione Gratuita</a></li>
+          <li><a hfref="">Canditati Ora</a></li>
         </ul>
       </nav>
