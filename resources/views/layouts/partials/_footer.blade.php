dd580dae62772883e3c930d22ad56ec80c745faa
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 17:19:12 +0100

    up

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,3 +1,9 @@
+    <nav>
+      <ul>
+        <li><a href="{{route('static_page.privacy')}}">Privacy</a></li>
+        <li><a href="{{route('static_page.faq')}}">Domande Frequenti</a></li>
+      </ul>
+    </nav>
   </div>
 </body>
 </html>
