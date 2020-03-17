4ea83379a35946ea31067b6d6990b5a34afd7be0
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 17:17:08 +0100

    up

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -10,10 +10,10 @@
     <div class="container">
       <nav>
         <ul>
-          <li><a hfref="">Home</a></li>
-          <li><a hfref="">Corso</a></li>
-          <li><a hfref="">Dopo Corso</a></li>
-          <li><a hfref="">Lezione Gratuita</a></li>
-          <li><a hfref="">Canditati Ora</a></li>
+          <li><a href="{{route('static_page.home')}}">Home</a></li>
+          <li><a href="">Corso</a></li>
+          <li><a href="">Dopo Corso</a></li>
+          <li><a href="">Lezione Gratuita</a></li>
+          <li><a href="">Canditati Ora</a></li>
         </ul>
       </nav>
--- resources/views/staticpages/index.blade.php
+++ resources/views/staticpages/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.layout')
 @section('header')
-    pippo
+    
 
 @endsection
