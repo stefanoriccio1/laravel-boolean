960881b1d49889856659a7c1ec96623a64960d5c
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 17:40:51 +0100

    up

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,9 +6,39 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
-    public function index(){
+    private $students;
 
+    public function _construct()
+    {
+      $this->students = [
+        [
+          'img'=> 'https://www.boolean.careers/images/students/biagini.png',
+          'nome' => 'Alessandro Biagini',
+          'eta' => 25,
+          'azienda' => 'DISC SPA',
+          'ruolo'=> 'web developer',
+          'descrizione'=> 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+        ],
+        [
+          'img'=> 'https://www.boolean.careers/images/students/poggini.png',
+          'nome' => 'Paola Poggini',
+          'eta' => 24,
+          'azienda' => 'Prima Assicurazioni',
+          'ruolo'=> 'junior software engineer',
+          'descrizione'=> 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.'
+        ],
+        [
+          'img'=> 'https://www.boolean.careers/images/students/masetti.png',
+          'nome' => 'Loana Masetti',
+          'eta' => 36,
+          'azienda' => 'The Zen Agency',
+          'ruolo'=> 'Web developer',
+          'descrizione'=> 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.'
+        ]
+      ];
+    }
+    public function index(){
+      dd($this->students);
     }
     public function show($id){
 
