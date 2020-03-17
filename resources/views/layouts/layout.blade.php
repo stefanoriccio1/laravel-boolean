ca1b0927e342aa74d65938570389f0d12c2f8f4d
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 16:28:21 +0100

    up

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,22 +1,17 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, intial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Laravel Boolean</title>
-  </head>
-  <body>
-    <div class="container">
+      @include('layouts.partials._header')
+      {{-- header --}}
       <header>
 
       </header>
+      {{-- /header --}}
+      {{-- main --}}
       <main>
 
       </main>
+      {{-- /main --}}
+      {{-- footer --}}
       <footer>
-        
+
       </footer>
-    </div>
-  </body>
-</html>
+      {{-- /footer --}}
+        @include('layouts.partials._footer')
