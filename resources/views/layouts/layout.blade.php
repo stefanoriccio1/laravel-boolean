c2932017c2aa62124c6929c8dfa913b732b4b7cd
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-17 16:23:16 +0100

    up

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Laravel Boolean</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  </body>
+</html>
