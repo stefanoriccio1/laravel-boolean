cf6faedfe75c9d6550a70fa21c87e57161110ac8
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-18 21:38:30 +0100

    up

--- config/students.php
+++ config/students.php
@@ -7,7 +7,8 @@ return[
       'eta' => 25,
       'azienda' => 'DISC SPA',
       'ruolo'=> 'web developer',
-      'descrizione'=> 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+      'descrizione'=> 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.',
+      'genere' => 'm'
     ],
     [
       'img'=> 'https://www.boolean.careers/images/students/poggini.png',
@@ -15,7 +16,8 @@ return[
       'eta' => 24,
       'azienda' => 'Prima Assicurazioni',
       'ruolo'=> 'junior software engineer',
-      'descrizione'=> 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.'
+      'descrizione'=> 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.',
+      'genere' => 'f'
     ],
     [
       'img'=> 'https://www.boolean.careers/images/students/masetti.png',
@@ -23,7 +25,8 @@ return[
       'eta' => 36,
       'azienda' => 'The Zen Agency',
       'ruolo'=> 'Web developer',
-      'descrizione'=> 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.'
+      'descrizione'=> 'Ha scoperto la passione per l\'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.',
+      'genere' => 'f'
     ]
   ]
 ];
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.layout')
 @section('main')
   @foreach (config('students.students') as $student)
-    {{-- {{$student['nome']}} --}}
+    <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+    <h2>
+      {{$student['nome']}}( {{$student['eta']}} anni )
+    </h2>
+    <h3>
+      Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}
+    </h3>
+    
   @endforeach
 @endsection
