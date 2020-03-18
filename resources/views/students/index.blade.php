ec3bfb8ac325082efec54a3766399094f31105f8
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-18 21:57:14 +0100

    up

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.layout')
 @section('main')
-  @foreach (config('students.students') as $student)
-    <img src="{{$student['img']}}" alt="{{$student['nome']}}">
-    <h2>
-      {{$student['nome']}}( {{$student['eta']}} anni )
-    </h2>
-    <h3>
-      Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}
-    </h3>
-    
-  @endforeach
+  <div class="students">
+    @foreach (config('students.students') as $student)
+      <div class="student">
+        <div class="info">
+          <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+          <div class="text">
+            <h2>
+              {{$student['nome']}}( {{$student['eta']}} anni )
+            </h2>
+            <h3>
+              Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}
+            </h3>
+          </div>
+        </div>
+        <p class = "description">{{$student['descrizione']}}</p>
+      </div>
+    @endforeach
+  </div>
 @endsection
