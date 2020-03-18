9b6c7c81f47f34cb57fe6821e884340804771c0b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-18 22:25:11 +0100

    up

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,1 +1,19 @@
-@dd($student)
+@extends('layouts.layout')
+@section('main')
+  <div class="students">
+      <div class="student">
+        <div class="info">
+          <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+          <div class="text">
+            <h2>
+              {{$student['nome']}}( {{$student['eta']}} anni )
+            </h2>
+            <h3>
+              Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} come {{$student['ruolo']}}
+            </h3>
+          </div>
+        </div>
+        <p class = "description">{{$student['descrizione']}}</p>
+      </div>
+  </div>
+@endsection
