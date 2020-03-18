f5d31c6178a0dce6599c8907b0052bbba6955558
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-18 19:45:27 +0100

    up

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, intial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <title>Laravel Boolean</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
     <div class="container">
-      <nav>
+      <nav class= "navbar">
         <ul>
           <li><a class="{{ (Request::route()->getName() == 'static_page.home') ? 'active' : ' '}}" href="{{route('static_page.home')}}">Home</a></li>
           <li><a href="">Corso</a></li>
